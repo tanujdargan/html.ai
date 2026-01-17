902ad6555ab17b8366614ac053cd1d006b7fb813
sultanalzoghaibi <ssultan007@icloud.com>
2026-01-17 17:18:48 -0500

    v1.05 test random Html rendering availible via "POST http://localhost:3000/tagAi"

--- htmlTag/aiBackend/server.py
+++ htmlTag/aiBackend/server.py
@@ -24,6 +24,24 @@ client = MongoClient("mongodb://mongo:27017/")  # works inside Docker
 mongodb = client["html_ai"]
 
 users_collection = mongodb["users"]  # collection
+
+
+users_collection.insert_one({
+            "user_id": "sultan",
+            "variants": {
+                "A": {
+                    "current_html": "<div>A</div>",
+                    "current_score": 4.3,
+                    "history": []
+                },
+
+                "B": {
+                    "current_html": "<div>B</div>",
+                    "current_score": 3.0,
+                    "history": []
+                }
+            }
+        })
 # ----------------------------------------
 # Payload Models
 # ----------------------------------------
@@ -62,7 +80,7 @@ def aiTag(user_id: str, changingHtml: str, contextHtml: str):
                 },
 
                 "B": {
-                    "current_html": changingHtml,
+                    "current_html": changingHtml, #TODO MUST BE DYNAMICs
                     "current_score": 3.0,
                     "history": []
                 }
@@ -78,6 +96,21 @@ def aiTag(user_id: str, changingHtml: str, contextHtml: str):
         }
 
     # TODO: ensure user exists + store HTML (you will add this later)
+    boolChoice:bool = bool( random.randint(0,1))
+
+    if boolChoice:
+        return_html = mongodb["users"].find_one({"user_id": user_id})["variants"]["A"]["current_html"]
+
+
+    else:
+        return_html = mongodb["users"].find_one({"user_id": user_id})["variants"]["B"]["current_html"]
+
+
+    return {
+        "status": "ok",
+        "changingHtml": return_html,
+    }
+
 
     return ABai(user_id, changingHtml, contextHtml)
 
@@ -92,9 +125,7 @@ def ABai(user_id: str, changingHtml: str, contextHtml: str):
     
     '''
 
-
-
-
+    boolChoice: bool = bool(random.randint(0, 1))
 
     return {
         "status": "ok",
@@ -131,20 +162,40 @@ def does_user_exists(user_id: str) -> bool:
 # ----------------------------------------
 # Logic: rewardTag
 # ----------------------------------------
+
+RERENDER_SCORE = 5
 def rewardTag(user_id: str, reward: float):
     """
     Store a reward value mapped to the given user.
     """
-    scores.insert_one({
-        "user_id": user_id,
-        "reward": reward
-    })
+
 
     return {
         "status": "reward_logged",
         "user_id": user_id,
         "reward": reward
     }
+
+def reRender(user_id, testLetter, contextHtml, oldHtml) -> str:
+    mongodb["users"].insert_one({"user_id": user_id})
+
+
+
+
+
+
+
+def ShouldWeReRender(user_id: str, changingHtml: str, contextHtml: str) -> bool:
+
+    AScore = mongodb["users"].find_one({"user_id": user_id})["variants"]["A"]["current_html"]
+    BScore = mongodb["users"].find_one({"user_id": user_id})["variants"]["B"]["current_html"]
+
+    if AScore > BScore + RERENDER_SCORE:
+        reRender(user_id, "A", contextHtml, AScore)
+
+
+
+
 
 
 # ----------------------------------------
